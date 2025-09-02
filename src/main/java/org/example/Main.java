a1e6efc1e449c4c73cdca910d5d5a6eaa8b21a65
HAROLD LOPEZ <harold_lopezfo@fet.edu.co>
2025-09-01 23:09:57 -0500

    agregar un listado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         CuentaBanco cuenta = new CuentaBanco();
-        cuenta.depositar(100);
-        cuenta.retirar(70);
+        cuenta.depositar(500);
+        cuenta.retirar(200);
         System.out.println("Saldo actual: " + cuenta.getSaldo());
     }
 }
